33a8274cedea318ecc94f71c79a75cb150293531
Viktória Nemkin <viktoria.nemkin@gmail.com>
2024-11-29 22:44:04 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false,
+    },
+  });
+
+  // Load Facebook Messenger
+  mainWindow.loadURL('https://www.messenger.com');
+
+  // Remove the default menu for a cleaner UI
+  mainWindow.setMenu(null);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    mainWindow = new BrowserWindow();
+  }
+});
